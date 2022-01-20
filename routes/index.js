e49d7bf0ff38d0713f0e932c12833d487cef8dee
Flo-314 <julianflo1916@gmail.com>
2022-01-20 17:21:44 -0300

    posts se pueden eliminar desde frontend de admin etc

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var router = express.Router();
-
+const Posts = require("../models/post");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { user: req.user });
+router.get("/", async (req, res, next) => {
+  let posts = await Posts.find({});
+
+  res.render("index", { user: req.user, posts });
 });
+
+router.post("/", async (req, res) => {
+  if(req.user){
+  await Post.findByIdAndRemove({ _id: req.body.postid });
+  let posts = await Post.find({}).populate("user");
+  res.render("index", { posts: posts, user: req.user });
+  }
+});
+
 router.get("/log-out", function (req, res, next) {
   if (req.user) {
     req.logout();
